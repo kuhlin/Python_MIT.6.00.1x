4cfe889476bc9804f626218b9ed2cbf43170fd28
kuhlin <kuhlin@gmail.com>
2018-09-20 19:03:31 +0200

    some changes

--- BabySteps/UnderstandingFloats/AproximateSolution.py
+++ BabySteps/UnderstandingFloats/AproximateSolution.py
@@ -22,7 +22,7 @@ while abs(guess**3 - cube) >= epsilon and guess <= cube:
     guess += increment
     num_guesses += 1
 print('num_guesses=', num_guesses)
-if abs(guess**3 -cube) >= epsilon:
+if abs(guess**3 - cube) >= epsilon:
     print('Failed on cube root of', cube)
 else:
     print(guess, 'is close to the cube root of', cube)
